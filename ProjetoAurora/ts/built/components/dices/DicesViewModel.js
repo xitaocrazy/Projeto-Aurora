9e9e46cb209b6088a763aa6808fe5531d80cfc27
Daniel de Souza Martins <daniel.martins@way2.com.br>
2018-01-07 23:25:53 -0200

    Enabling or disabling the send button
    Enabling or disabling the send button according with the dices information.

--- ProjetoAurora/ts/built/components/dices/DicesViewModel.js
+++ ProjetoAurora/ts/built/components/dices/DicesViewModel.js
@@ -2,12 +2,36 @@ var KnockoutComponents;
 (function (KnockoutComponents) {
     var DicesViewModel = /** @class */ (function () {
         function DicesViewModel() {
+            this.setDefaultValues();
+            this.setComputeds();
+        }
+        DicesViewModel.prototype.setDefaultValues = function () {
             this.dice1 = ko.observable('');
             this.dice2 = ko.observable('');
             this.dice3 = ko.observable('');
             this.dice4 = ko.observable('');
             this.dice5 = ko.observable('');
-        }
+        };
+        DicesViewModel.prototype.setComputeds = function () {
+            this.canSend = ko.computed(this.verifyIfCanSend, this, { disposeWhenNodeIsRemoved: true });
+        };
+        DicesViewModel.prototype.verifyIfCanSend = function () {
+            return this.dice1() != '' &&
+                this.dice2() != '' &&
+                this.dice3() != '' &&
+                this.dice4() != '' &&
+                this.dice5() != '' &&
+                this.dice1() != undefined &&
+                this.dice2() != undefined &&
+                this.dice3() != undefined &&
+                this.dice4() != undefined &&
+                this.dice5() != undefined &&
+                this.dice1() != null &&
+                this.dice2() != null &&
+                this.dice3() != null &&
+                this.dice4() != null &&
+                this.dice5() != null;
+        };
         DicesViewModel.prototype.clean = function () {
             this.dice1('');
             this.dice2('');
--- ProjetoAurora/ts/src/components/dices/DicesViewModel.ts
+++ ProjetoAurora/ts/src/components/dices/DicesViewModel.ts
@@ -5,13 +5,43 @@ module KnockoutComponents{
         dice3: KnockoutObservable<string>;
         dice4: KnockoutObservable<string>;
         dice5: KnockoutObservable<string>;
+        canSend: KnockoutComputed<boolean>;
 
         constructor() {
+             this.setDefaultValues();
+             this.setComputeds();
+        }
+
+        private setDefaultValues(){
             this.dice1 = ko.observable(''); 
             this.dice2 = ko.observable('');
             this.dice3 = ko.observable(''); 
             this.dice4 = ko.observable('');
-            this.dice5 = ko.observable(''); 
+            this.dice5 = ko.observable('');
+        }
+
+        private setComputeds(){
+            this.canSend = ko.computed(this.verifyIfCanSend, this, { disposeWhenNodeIsRemoved: true });
+        }
+
+        private verifyIfCanSend(){
+            return this.dice1() != '' &&
+                   this.dice2() != '' &&
+                   this.dice3() != '' &&
+                   this.dice4() != '' &&
+                   this.dice5() != '' &&
+
+                   this.dice1() != undefined &&
+                   this.dice2() != undefined &&
+                   this.dice3() != undefined &&
+                   this.dice4() != undefined &&
+                   this.dice5() != undefined &&
+
+                   this.dice1() != null &&
+                   this.dice2() != null &&
+                   this.dice3() != null &&
+                   this.dice4() != null &&
+                   this.dice5() != null;
         }
 
         clean(){
